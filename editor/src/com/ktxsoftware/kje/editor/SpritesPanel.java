f86e93741f6febeb8c3c1f6b3429495c4c7df85f
Robert Konrad <robert@ktx-software.com>
2014-08-24 11:36:38 +0200

    Edited level.

--- editor/src/com/ktxsoftware/kje/editor/SpritesPanel.java
+++ editor/src/com/ktxsoftware/kje/editor/SpritesPanel.java
@@ -30,25 +30,25 @@ public class SpritesPanel extends JPanel implements MouseListener, MouseMotionLi
 	}
 
 	private SpritesPanel() {
-		sprites.add(new Sprite("../Assets/Graphics/agent.png", 0, 19, 49));
+		//sprites.add(new Sprite("../Assets/Graphics/agent.png", 0, 19, 49));
 		sprites.add(new Sprite("../Assets/Graphics/professor.png", 1, 20, 52));
-		sprites.add(new Sprite("../Assets/Graphics/rowdy.png", 2, 25, 52));
-		sprites.add(new Sprite("../Assets/Graphics/mechanic.png", 3, 21, 52));
-		sprites.add(new Sprite("../Assets/Graphics/door.png", 4, 16, 64));
-		sprites.add(new Sprite("../Assets/Graphics/soldier.png", 5, 22, 41));
-		sprites.add(new Sprite("../Assets/Graphics/window.png", 6, 16, 80));
-		sprites.add(new Sprite("../Assets/Graphics/gate.png", 7, 16, 96));
-		sprites.add(new Sprite("../Assets/Graphics/gatter.png", 8, 32, 6));
-		sprites.add(new Sprite("../Assets/Graphics/computer.png", 9, 46, 60));
-		sprites.add(new Sprite("../Assets/Graphics/machinegun.png", 10, 42, 41));
-		sprites.add(new Sprite("../Assets/Graphics/boss.png", 11, 26, 35));
-		sprites.add(new Sprite("../Assets/Graphics/car.png", 12, 100, 41));
+		//sprites.add(new Sprite("../Assets/Graphics/rowdy.png", 2, 25, 52));
+		//sprites.add(new Sprite("../Assets/Graphics/mechanic.png", 3, 21, 52));
+		//sprites.add(new Sprite("../Assets/Graphics/door.png", 4, 16, 64));
+		//sprites.add(new Sprite("../Assets/Graphics/soldier.png", 5, 22, 41));
+		//sprites.add(new Sprite("../Assets/Graphics/window.png", 6, 16, 80));
+		//sprites.add(new Sprite("../Assets/Graphics/gate.png", 7, 16, 96));
+		//sprites.add(new Sprite("../Assets/Graphics/gatter.png", 8, 32, 6));
+		//sprites.add(new Sprite("../Assets/Graphics/computer.png", 9, 46, 60));
+		//sprites.add(new Sprite("../Assets/Graphics/machinegun.png", 10, 42, 41));
+		//sprites.add(new Sprite("../Assets/Graphics/boss.png", 11, 26, 35));
+		//sprites.add(new Sprite("../Assets/Graphics/car.png", 12, 100, 41));
 		addMouseMotionListener(this);
 		addMouseListener(this);
 	}
 	
 	public Sprite getSprite(int index) {
-		return sprites.get(index);
+		return sprites.get(0);
 	}
 	
 	public int getSpriteCount() {
