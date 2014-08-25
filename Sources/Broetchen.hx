dd5318fb5a58cda3e2543abc5c400b8c11c47cec
Ingo Jakobs <ingo.jakobs@stud.tu-darmstadt.de>
2014-08-25 21:04:13 +0200

    ZornigeFrau

--- Sources/Broetchen.hx
+++ Sources/Broetchen.hx
@@ -1,23 +1,24 @@
 package;
 
+import kha.graphics2.Graphics;
 import kha.Image;
 import kha.Loader;
 import kha.Sprite;
 
 class Broetchen extends Sprite {
 	public function new() {
-		super(Loader.the.getImage('broetchen1')); // TODO!!!
+		super(Loader.the.getImage('broetchen1'));
 		this.speedx = 4;
 		accy = 0;
 	}
 	
-	override public function update():Void {
-		super.update();
-		
+	override public function render(g:Graphics):Void 
+	{
 		if (Cfg.getVictoryCondition(VictoryCondition.MEHRKORN)) {
 			image = Loader.the.getImage("broetchen1");
 		} else {
 			image = Loader.the.getImage("broetchen2");
 		}
+		super.render(g);
 	}
 }
--- Sources/Weib.hx
+++ Sources/Weib.hx
@@ -1,13 +1,15 @@
 package;
 
+import kha.Animation;
 import kha.Loader;
 import kha.Sprite;
 
 class Weib extends Sprite {
 	public function new(x: Float, y: Float) {
-		super(Loader.the.getImage('mechanic'), 64, 64); // TODO!!!
-		this.x = x - 32;
-		this.y = y - 5;
+		super(Loader.the.getImage('ZornigeFrau'), 2 * 30, 2 * 25); // TODO!!!
+		setAnimation(new Animation([0,0,1,2,1,3,4,5,6,5,4,3,1,2,7,7,2],15));
+		this.x = x - 16;
+		this.y = y + 5;
 		accy = 0;
 	}
 	
