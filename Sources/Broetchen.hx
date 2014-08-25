54ef75c3046469535b0c98f4cb882f2c643308e8
Ingo Jakobs <ingo.jakobs@stud.tu-darmstadt.de>
2014-08-25 20:43:08 +0200

    Brötchen

--- Sources/Broetchen.hx
+++ Sources/Broetchen.hx
@@ -1,12 +1,23 @@
 package;
 
+import kha.Image;
 import kha.Loader;
 import kha.Sprite;
 
 class Broetchen extends Sprite {
 	public function new() {
-		super(Loader.the.getImage('euro')); // TODO!!!
+		super(Loader.the.getImage('broetchen1')); // TODO!!!
 		this.speedx = 4;
 		accy = 0;
+	}
+	
+	override public function update():Void {
+		super.update();
+		
+		if (Cfg.getVictoryCondition(VictoryCondition.MEHRKORN)) {
+			image = Loader.the.getImage("broetchen1");
+		} else {
+			image = Loader.the.getImage("broetchen2");
+		}
 	}
 }
