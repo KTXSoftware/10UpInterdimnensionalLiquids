cc3366daaabb4a36e4335e51f8ac6782a3ec5784
Robert Konrad <robert@ktx-software.com>
2014-08-25 12:15:00 +0200

    Adjust start dialogue to previously played game.

--- Sources/Verkaeuferin.hx
+++ Sources/Verkaeuferin.hx
@@ -9,9 +9,15 @@ import kha.Rectangle;
 import kha.Scene;
 
 class Verkaeuferin extends Player {
+	private static var me: Verkaeuferin;
+	
+	public static function the(): Verkaeuferin {
+		return me;
+	}
 	
 	public function new(x: Float, y: Float) {
 		super(x, y - 8, "mechanic", Std.int(410 / 10) * 2, Std.int(455 / 7) * 2);
+		me = this;
 		Player.setPlayer(2, this);
 		collider = new Rectangle(20, 30, 41 * 2 - 40, (65 - 1) * 2 - 30);
 		walkLeft = Animation.createRange(11, 18, 4);
--- Sources/ZeroEightFifteenMan.hx
+++ Sources/ZeroEightFifteenMan.hx
@@ -9,9 +9,15 @@ import kha.Rectangle;
 import kha.Scene;
 
 class ZeroEightFifteenMan extends Player {
+	private static var me: ZeroEightFifteenMan;
+	
+	public static function the(): ZeroEightFifteenMan {
+		return me;
+	}
 	
 	public function new(x: Float, y: Float) {
 		super(x, y - 8, "professor", Std.int(410 / 10 * 2), Std.int(455 / 7 * 2));
+		me = this;
 		Player.setPlayer(1, this);
 				
 		collider = new Rectangle(10 * 2, 15 * 2, (41 - 20) * 2, ((65 - 1) - 15) * 2);
