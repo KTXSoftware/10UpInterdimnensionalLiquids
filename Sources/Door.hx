80ae1c7684ba0438652f38a0e27f0e26fc218176
Robert Konrad <robert@ktx-software.com>
2014-08-25 17:29:09 +0200

    Mafioso gets angry when you enter his room.

--- Sources/Door.hx
+++ Sources/Door.hx
@@ -5,14 +5,18 @@ import kha.Loader;
 import kha.Sprite;
 
 class Door extends Sprite {
-	public function new(x: Float, y: Float) {
+	private var backdoor: Bool;
+	
+	public function new(x: Float, y: Float, backdoor: Bool) {
 		super(Loader.the.getImage('door'), Std.int(128 * 2 / 4), 64 * 2, 0);
 		this.x = x;
 		this.y = y;
+		this.backdoor = backdoor;
 	}
 	
 	override public function hit(sprite: Sprite): Void {
 		super.hit(sprite);
 		setAnimation(Animation.create(1));
+		if (backdoor) Mafioso.the().useMg = true;
 	}
 }
